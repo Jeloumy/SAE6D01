03115b4a43e38059b3239955f6e49fb58df59536
Jeloumy <jeremy60.duflot60@gmail.com>
2024-05-21 17:03:10 +0200

    feat: ajout carte Leaflet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateProfileComponent } from '../app/pages/create-profile/create-profile.component';
+import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
   { path: 'create-profile', component: CreateProfileComponent },
+  { path: 'search', component: SearchComponent },
   // autres routes...
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateProfileComponent } from './pages/create-profile/create-profile.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MapComponent } from './pages/map/map.component';
+import { HandicapFormComponent } from './pages/handicap-form/handicap-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateProfileComponent
+    CreateProfileComponent,
+    SearchComponent,
+    HandicapFormComponent,
+    MapComponent,
   ],
   imports: [
     BrowserModule,
