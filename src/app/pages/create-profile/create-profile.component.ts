586327c26f443b81eb4c2be4e35cbb6964396f91
Jeloumy <jeremy60.duflot60@gmail.com>
2024-05-22 10:00:25 +0200

    style : aperçu profil sélectionné + fix : relancement d'appli reprise du dernier profil sélectionné

--- src/app/pages/create-profile/create-profile.component.ts
+++ src/app/pages/create-profile/create-profile.component.ts
@@ -11,26 +11,39 @@ export class CreateProfileComponent implements OnInit {
   profile: UserProfile = { id: 0, pseudo: '', typeHandicap: '' };
   profiles: UserProfile[] = [];
   editingProfile: UserProfile | null = null;
+  currentProfile: UserProfile | null = null;
 
   constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
     this.loadProfiles();
+    this.loadCurrentProfile();
   }
 
   loadProfiles(): void {
     this.profiles = this.profileService.getProfilesList();
-    if (this.profiles.length === 1) {
-      this.selectProfile(this.profiles[0]);
+  }
+
+  loadCurrentProfile(): void {
+    const storedProfile = this.profileService.getCurrentProfile();
+    if (storedProfile) {
+      const matchedProfile = this.profiles.find(profile => profile.id === storedProfile.id);
+      if (matchedProfile) {
+        this.selectProfile(matchedProfile, false); // Sélection automatique sans pop-up
+      }
     }
   }
 
   createProfile(): void {
+    const isInitialProfile = this.profiles.length === 0; // Vérifie s'il n'y a pas de profils existants
     if (this.editingProfile) {
       this.profileService.updateProfile(this.profile);
       this.editingProfile = null;
     } else {
       this.profileService.createProfile(this.profile);
+      if (isInitialProfile) {
+        this.selectProfile(this.profile, false); // Sélectionne automatiquement le nouveau profil
+      }
     }
     this.profile = { id: 0, pseudo: '', typeHandicap: '' };
     this.loadProfiles();
@@ -39,14 +52,25 @@ export class CreateProfileComponent implements OnInit {
   deleteProfile(profileId: number): void {
     this.profileService.deleteProfile(profileId);
     this.loadProfiles();
+    if (this.currentProfile?.id === profileId) {
+      this.currentProfile = null;
+      this.profileService.setCurrentProfile(null);
+    }
   }
 
   editProfile(profile: UserProfile): void {
-    this.profile = {...profile};
+    this.profile = { ...profile };
     this.editingProfile = profile;
   }
 
-  selectProfile(profile: UserProfile): void {
+  selectProfile(profile: UserProfile, confirmChange: boolean = true): void {
+    if (confirmChange && this.currentProfile && this.currentProfile.id !== profile.id) {
+      const userConfirmed = confirm("Êtes-vous sûr de vouloir changer de profil ?");
+      if (!userConfirmed) {
+        return;
+      }
+    }
+    this.currentProfile = profile;
     this.profileService.setCurrentProfile(profile);
   }
 }
