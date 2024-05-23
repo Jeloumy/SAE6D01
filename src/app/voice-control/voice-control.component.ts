c96d4d21ed470c4c929c80a235e2be4c3c6bac56
EmmaCunienq <3hda07@gmail.com>
2024-05-23 09:43:01 +0200

    Create component using voice recognition service

---
+++ src/app/voice-control/voice-control.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SpeechRecognitionService } from '../speech-recognition.service';
+
+@Component({
+  selector: 'app-voice-control',
+  templateUrl: './voice-control.component.html',
+  styleUrls: ['./voice-control.component.css']
+})
+export class VoiceControlComponent implements OnInit, OnDestroy {
+  public message: string = '';
+  private commandList: { [key: string]: () => void } = {
+    'aller à la page d\'accueil': () => this.navigateHome(),
+    'ouvrir le menu': () => this.openMenu(),
+    'fermer le menu': () => this.closeMenu()
+  };
+
+  constructor(private speechRecognitionService: SpeechRecognitionService) {}
+
+  ngOnInit() {
+    this.speechRecognitionService.onResult((text: string) => {
+      this.message = text;
+      this.handleCommand(text);
+    });
+
+    this.speechRecognitionService.onError((event: any) => {
+      console.error('Speech recognition error', event);
+    });
+
+    this.speechRecognitionService.onEnd(() => {
+      console.log('Speech recognition ended');
+    });
+
+    this.startListening();
+  }
+
+  ngOnDestroy() {
+    this.stopListening();
+  }
+
+  startListening() {
+    this.speechRecognitionService.start();
+  }
+
+  stopListening() {
+    this.speechRecognitionService.stop();
+  }
+
+  handleCommand(command: string) {
+    if (this.commandList[command]) {
+      this.commandList[command]();
+    } else {
+      console.log(`Command not recognized: ${command}`);
+    }
+  }
+
+  navigateHome() {
+    console.log('Navigating to home page...');
+    // Implement your navigation logic here
+  }
+
+  openMenu() {
+    console.log('Opening menu...');
+    // Implement your menu opening logic here
+  }
+
+  closeMenu() {
+    console.log('Closing menu...');
+    // Implement your menu closing logic here
+  }
+}
