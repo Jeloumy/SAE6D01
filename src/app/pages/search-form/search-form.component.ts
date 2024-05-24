f69e7ae6f96f2557ceca5e40ebb3584017651ce9
Jeloumy <jeremy60.duflot60@gmail.com>
2024-05-24 10:00:58 +0200

    feat : ajout de la carte et de l'api
    implémentation du code de Ewan pour l'api + implémentation du code de Félix pour visualiser les données sur une carte

--- src/app/pages/search-form/search-form.component.ts
+++ src/app/pages/search-form/search-form.component.ts
@@ -1,18 +1,50 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Handicap, DispositifLieu } from '../../models/user-profile';
 import { ProfileService } from '../../services/profile/profile.service';
+import { AccesLibreService } from '../../services/acces-libre/acces-libre.service';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.scss']
 })
-export class SearchFormComponent {
+export class SearchFormComponent implements OnInit {
   searchQuery: string = '';
   selectedHandicaps: Handicap[] = [];
   selectedDispositifLieu: DispositifLieu[] = [];
+  results: any;
 
-  constructor(private profileService: ProfileService) {}
+  @Output() searchEvent = new EventEmitter<any>();
+
+  private dispositifMapping: { [key: string]: string } = {
+    'Chemin vers l\'accueil accessible': 'having_accessible_exterior_path',
+    'Dispositif d\'appel à l\'entrée': 'having_entry_call_device',
+    'Proximité de l\'accueil': 'having_visible_reception',
+    'Présence de personnel': 'having_staff',
+    'Personnel sensibilisé ou formé': 'having_trained_staff',
+    'Audiodescription': 'having_audiodescription',
+    'Equipements spécifiques pour personne malentendante': 'having_hearing_equipments',
+    'Chemin sans rétrécissement jusqu\'à l\'accueil ou information inconnue': 'having_entry_no_shrink',
+    'Chambre accessible': 'having_accessible_rooms',
+    'Toilettes PMR': 'having_adapted_wc',
+    'Établissement labellisé': 'having_label',
+    'Stationnement à proximité': 'having_parking',
+    'Transport en commun à proximité': 'having_public_transportation',
+    'Stationnement PMR (dans l\'établissement ou à proximité)': 'having_adapted_parking',
+    'Maximum une marche à l\'entrée': 'having_path_low_stairs',
+    'Maximum une marche à l\'accueil': 'having_entry_low_stairs',
+    'Entrée accessible': 'having_accessible_entry',
+    'Largeur de porte supérieure à 80cm ou information inconnue': 'having_entry_min_width',
+    'Entrée spécifique PMR': 'having_adapted_entry',
+    'Balise sonore': 'having_sound_beacon',
+    'Pas de chemin extérieur ou information inconnue': 'having_no_path',
+    'Chemin adapté aux personnes mal marchantes': 'having_adapted_path',
+    'Extérieur - plain-pied ou accessible via rampe ou ascenseur': 'having_reception_low_stairs',
+    'Chemin extérieur accessible': 'having_accessible_path_to_reception',
+    'Extérieur - bande de guidage': 'having_guide_band'
+  };
+
+  constructor(private profileService: ProfileService, private accesLibreService: AccesLibreService) {}
 
   ngOnInit(): void {
     this.loadProfilePreferences();
@@ -27,7 +59,21 @@ export class SearchFormComponent {
   }
 
   onSearch(): void {
-    // Logique de recherche à implémenter
-    console.log('Recherche effectuée:', this.searchQuery, this.selectedHandicaps, this.selectedDispositifLieu);
+    const dispositifsFiltered = this.selectedDispositifLieu
+      .map(d => this.dispositifMapping[d.name])
+      .filter(d => d); // Filtre les valeurs vides
+  
+    const filters = {
+      query: this.searchQuery,
+      dispositifs: dispositifsFiltered
+    };
+  
+    console.log('Filters:', filters);
+  
+    this.accesLibreService.getErp(filters).subscribe(data => {
+      this.results = data;
+      console.log('API Response:', data);
+      this.searchEvent.emit(data); // Émettez les résultats
+    });
   }
 }
--- src/app/services/acces-libre/acces-libre.service.ts
+++ src/app/services/acces-libre/acces-libre.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccesLibreService {
+  private apiUrl = 'https://acceslibre.beta.gouv.fr/api/erps';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getErp(filters: any): Observable<any> {
+    let params = new HttpParams();
+    if (filters.query) params = params.set('q', filters.query);
+    if (filters.dispositifs && filters.dispositifs.length > 0) {
+      filters.dispositifs.forEach((equipment: string) => {
+          params = params.append('equipments', equipment);
+      });
+    }
+
+    const headers = new HttpHeaders({
+      'accept': 'application/json',
+      'Authorization': `Api-Key oU6JwJ1T.himZfVvhB5F0JqY6YeU2A2cDbgbr0tzN`
+    });
+
+    console.log('Params:', params.toString());
+
+    return this.http.get<any>(this.apiUrl, { params, headers });
+  }
+
+  getResultsByUrl(url: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'accept': 'application/json',
+      'Authorization': `Api-Key oU6JwJ1T.himZfVvhB5F0JqY6YeU2A2cDbgbr0tzN`
+    });
+
+    return this.http.get<any>(url, { headers });
+  }
 }
