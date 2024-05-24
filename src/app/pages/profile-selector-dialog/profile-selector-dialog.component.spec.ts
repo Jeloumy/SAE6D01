92c2e4afb352c31b85f1d1dfe2f1a3eedc0d5608
ewan-questel <ew.questel@gmail.com>
2024-05-24 14:51:55 +0200

    refactor
    reviewed the code to remove unecessary lines, also added arias to the html components

--- src/app/pages/profile-selector-dialog/profile-selector-dialog.component.spec.ts
+++ src/app/pages/profile-selector-dialog/profile-selector-dialog.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProfileSelectorDialogComponent } from './profile-selector-dialog.component';
 
 describe('ProfileSelectorDialogComponent', () => {
--- src/app/services/acces-libre/acces-libre.service.spec.ts
+++ src/app/services/acces-libre/acces-libre.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AccesLibreService } from './acces-libre.service';
 
 describe('AccesLibreService', () => {
