204993a01650ba43473493d44da0262724271a41
Jeloumy <jeremy60.duflot60@gmail.com>
2024-06-10 10:40:00 +0200

    fix : search-component

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -7,12 +7,9 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
   searchResults: any;
-  filters: any = {}; 
 
-  onSearchResults(results: any, filters: any): void { 
+  onSearchResults(results: any): void {
     this.searchResults = results;
-    this.filters = filters; 
     console.log('Résultats de la recherche:', results);
-    console.log('Filtres de la recherche:', filters);
   }
 }
