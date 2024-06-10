cd2b5e343a51680c5bedfff8841027f43a9d548b
Jeloumy <jeremy60.duflot60@gmail.com>
2024-06-10 14:48:34 +0200

    feat : add geolocalisation + search around me

--- src/app/pages/search-form/search-form.component.ts
+++ src/app/pages/search-form/search-form.component.ts
@@ -17,31 +17,7 @@ export class SearchFormComponent implements OnInit {
   @Output() searchEvent = new EventEmitter<any>();
 
   private dispositifMapping: { [key: string]: string } = {
-    'Chemin vers l\'accueil accessible': 'having_accessible_exterior_path',
-    'Dispositif d\'appel à l\'entrée': 'having_entry_call_device',
-    'Proximité de l\'accueil': 'having_visible_reception',
-    'Présence de personnel': 'having_staff',
-    'Personnel sensibilisé ou formé': 'having_trained_staff',
-    'Audiodescription': 'having_audiodescription',
-    'Equipements spécifiques pour personne malentendante': 'having_hearing_equipments',
-    'Chemin sans rétrécissement jusqu\'à l\'accueil ou information inconnue': 'having_entry_no_shrink',
-    'Chambre accessible': 'having_accessible_rooms',
-    'Toilettes PMR': 'having_adapted_wc',
-    'Établissement labellisé': 'having_label',
-    'Stationnement à proximité': 'having_parking',
-    'Transport en commun à proximité': 'having_public_transportation',
-    'Stationnement PMR (dans l\'établissement ou à proximité)': 'having_adapted_parking',
-    'Maximum une marche à l\'entrée': 'having_path_low_stairs',
-    'Maximum une marche à l\'accueil': 'having_entry_low_stairs',
-    'Entrée accessible': 'having_accessible_entry',
-    'Largeur de porte supérieure à 80cm ou information inconnue': 'having_entry_min_width',
-    'Entrée spécifique PMR': 'having_adapted_entry',
-    'Balise sonore': 'having_sound_beacon',
-    'Pas de chemin extérieur ou information inconnue': 'having_no_path',
-    'Chemin adapté aux personnes mal marchantes': 'having_adapted_path',
-    'Extérieur - plain-pied ou accessible via rampe ou ascenseur': 'having_reception_low_stairs',
-    'Chemin extérieur accessible': 'having_accessible_path_to_reception',
-    'Extérieur - bande de guidage': 'having_guide_band'
+    // Mapping des dispositifs
   };
 
   constructor(private profileService: ProfileService, private accesLibreService: AccesLibreService) {}
@@ -62,18 +38,48 @@ export class SearchFormComponent implements OnInit {
     const dispositifsFiltered = this.selectedDispositifLieu
       .map(d => this.dispositifMapping[d.name])
       .filter(d => d); // Filtre les valeurs vides
-  
+
     const filters = {
       query: this.searchQuery,
       dispositifs: dispositifsFiltered
     };
-  
+
     console.log('Filters:', filters);
-  
+
     this.accesLibreService.getErp(filters).subscribe(data => {
       this.results = data;
       console.log('API Response:', data);
       this.searchEvent.emit(data); // Émettez les résultats
     });
+  }
+
+  onSearchAroundMe(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const dispositifsFiltered = this.selectedDispositifLieu
+          .map(d => this.dispositifMapping[d.name])
+          .filter(d => d); // Filtre les valeurs vides
+
+        const filters = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          dispositifs: dispositifsFiltered
+        };
+
+        console.log('Filters for search around me:', filters);
+
+        this.accesLibreService.getErp(filters).subscribe(data => {
+          this.results = data;
+          console.log('API Response:', data);
+          this.searchEvent.emit(this.results); // Émettez les résultats
+        });
+      }, error => {
+        console.error('Geolocation error:', error);
+        // Gérer les erreurs de géolocalisation
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+      // Gérer le cas où la géolocalisation n'est pas supportée
+    }
   }
 }
--- src/app/services/acces-libre/acces-libre.service.ts
+++ src/app/services/acces-libre/acces-libre.service.ts
@@ -15,8 +15,13 @@ export class AccesLibreService {
     if (filters.query) params = params.set('q', filters.query);
     if (filters.dispositifs && filters.dispositifs.length > 0) {
       filters.dispositifs.forEach((equipment: string) => {
-          params = params.append('equipments', equipment);
+        params = params.append('equipments', equipment);
       });
+    }
+    if (filters.latitude && filters.longitude) {
+      const { minLat, minLon, maxLat, maxLon } = getBoundingBox(filters.latitude, filters.longitude, 20);
+      const zone = `${minLon},${minLat},${maxLon},${maxLat}`;
+      params = params.set('zone', zone);
     }
 
     const headers = new HttpHeaders({
@@ -38,3 +43,26 @@ export class AccesLibreService {
     return this.http.get<any>(url, { headers });
   }
 }
+
+function getBoundingBox(lat: number, lon: number, distanceInKm: number) {
+  const earthRadius = 6371; // Radius of the Earth in km
+
+  const latRad = lat * Math.PI / 180;
+  const lonRad = lon * Math.PI / 180;
+
+  const distanceRad = distanceInKm / earthRadius;
+
+  const minLat = lat - (distanceRad * 180 / Math.PI);
+  const maxLat = lat + (distanceRad * 180 / Math.PI);
+  const minLon = lon - (distanceRad * 180 / Math.PI) / Math.cos(latRad);
+  const maxLon = lon + (distanceRad * 180 / Math.PI) / Math.cos(latRad);
+
+  return {
+    minLat,
+    minLon,
+    maxLat,
+    maxLon
+  };
+}
+
+
