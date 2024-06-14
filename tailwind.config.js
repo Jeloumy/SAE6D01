24e78a01548080977cd75ed5a005805b1ed74f90
Valentin-Dbs <valentin.debs30@gmail.com>
2024-06-14 10:13:00 +0200

    update Search pour le rendre responsive mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,22 +13,19 @@ module.exports = {
     themes: [
       {
         hightContrast:{
-          'primary': '#000000',
-          'neutral': '#000000',
-          'success': '#ffffff',
-          'success-content':'#000',
-          'info': '#000',
-          'info-content':'#fff',
-          'accent':'#000',
+          'primary': '#000000', //Always black
+          'success': '#ffffff', //Background
+          'info': '#000', //Black to White
+          'info-content':'#fff', //White to Black
+          'accent':'#000', //Button
         },
       },
       {
         light:{
-          'success': '#134E4A',
-          'success-content': '#ffffff',
-          'accent': '#2DD4BF',
-          'info':'#fff',
-          'info-content':'#000',
+          'success': '#134E4A', //Background
+          'accent': '#2DD4BF', //Button
+          'info':'#fff', //Black to White
+          'info-content':'#000', //White to Black
         },
       },
     ],
