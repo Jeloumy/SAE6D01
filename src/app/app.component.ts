5ee58a0318f4eaf44f82f20567387b53ac424299
EmmaCunienq <113907832+EmmaCunienq@users.noreply.github.com>
2024-06-23 00:09:35 +0200

    fix : vocal search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor(
     private profileService: ProfileService,
     private themeService: ThemeService,
-    private speechService: SpeechService
+    private speechService: SpeechService,
   ) {
   }
 
--- src/app/services/speech/speech.service.ts
+++ src/app/services/speech/speech.service.ts
@@ -23,67 +23,53 @@ export class SpeechService {
   }
 
   async startListening(): Promise<void> {
-    const available = await SpeechRecognition['available']();
-    console.log('Speech recognition available:', available);
-    if (!available) {
-      console.log('Speech recognition not available');
-      return;
-    }
-
-    const hasPermission = await this.checkPermission();
-    console.log('hasPermission: ', JSON.stringify(hasPermission));
-    if (hasPermission.speechRecognition !== 'granted') {
-      console.log(
-        "Vous devez autoriser l'utilisation du micro par l'application pour utiliser la commande vocale"
-      );
-      return;
-    }
-
-    console.log('je vais entrer dans start');
-    SpeechRecognition.start({
-      language: 'fr-FR',
-      maxResults: 5,
-      prompt: 'Say something',
-      popup: true,
-      partialResults: true,
-    })
-      .then((result: any) => {
-        console.log('Recognition result:', JSON.stringify(result));
-
-        const navigation: { [key: string]: string } = {
-          recherche: '',
-          'nouveau profil': 'create-profile',
-          'modifier profil':
-            'edit-profile/' + this.profileService.getCurrentProfile()?.id,
-        };
-
-        for (const match of result.matches) {
-          for (const page in navigation) {
-            const regex = new RegExp(page); // Convertir la chaîne de caractères en regex
-            if (regex.test(match.toLowerCase())) {
-              this.router.navigate([navigation[page]]);
-            }
-          }
-          const motRecherche = 'chercher';
-          const regex = new RegExp(motRecherche);
-          if (this.router.url === '/' && regex.test(match.toLowerCase())) {
-            let index = match.indexOf(motRecherche);
-
-            if (index !== -1) {
-              let result = match.substring(index + motRecherche.length).trim();
-              console.log(result);
-            } else {
-              console.log(
-                "Le mot 'chercher' n'a pas été trouvé dans la phrase."
-              );
-            }
-            console.log('je suis dans recherche');
-          }
-        }
+      const available = await SpeechRecognition['available']();
+      console.log('Speech recognition available:', available);
+      if (!available) {
+        console.log('Speech recognition not available');
+        return;
+      }
+  
+      const hasPermission = await this.checkPermission();
+      console.log('hasPermission: ', JSON.stringify(hasPermission));
+      if (hasPermission.speechRecognition !== 'granted') {
+        console.log(
+          "Vous devez autoriser l'utilisation du micro par l'application pour utiliser la commande vocale"
+        );
+        return;
+      }
+  
+      SpeechRecognition.start({
+        language: 'fr-FR',
+        maxResults: 5,
+        prompt: 'Say something',
+        popup: true,
+        partialResults: true,
       })
-      .catch((error: any) => {
-        console.error('Recognition error:', error);
-      });
+        .then((result: any) => {
+          console.log('Recognition result:', JSON.stringify(result));
+  
+          const navigation: { [key: string]: string } = {
+            recherche: '',
+            'nouveau profil': 'create-profile',
+            'modifier profil':
+              'edit-profile/' + this.profileService.getCurrentProfile()?.id,
+          };
+  
+          for (const match of result.matches) {
+            for (const page in navigation) {
+              const regex = new RegExp(page); // Convertir la chaîne de caractères en regex
+              if (regex.test(match.toLowerCase())) {
+                this.router.navigate([navigation[page]]);
+              }
+            }
+          }
+        })
+        .catch((error: any) => {
+          console.error('Recognition error:', error);
+        });
+    
+    
   }
 
   stopListening(): void {
@@ -98,5 +84,37 @@ export class SpeechService {
 
   requestPermissions() {
     return SpeechRecognition.requestPermissions();
+  }
+
+  async listenForSearch(): Promise<string> {
+    const available = await SpeechRecognition['available']();
+    if (!available) {
+      console.error('Speech recognition not available');
+      return '';
+    }
+
+    const hasPermission = await this.checkPermission();
+    if (hasPermission.speechRecognition !== 'granted') {
+      console.error('Permission not granted for speech recognition');
+      return '';
+    }
+
+    return new Promise((resolve, reject) => {
+      SpeechRecognition.start({
+        language: 'fr-FR',
+        maxResults: 1,
+        prompt: 'Dites votre recherche',
+        popup: true,
+        partialResults: false,
+      })
+        .then((result: any) => {
+          const query = result.matches[0];
+          resolve(query);
+        })
+        .catch((error: any) => {
+          console.error('Speech recognition error:', error);
+          reject(error);
+        });
+    });
   }
 }
