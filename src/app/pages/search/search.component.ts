2814e349e41ceba7598528096c62a5cdff7a14a8
EmmaCunienq <113907832+EmmaCunienq@users.noreply.github.com>
2024-06-24 01:22:07 +0200

    feat : continuous listening

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SearchFormComponent } from '../../components/search-form/search-form.component';
+import { ProfileService } from '../../services/profile/profile.service';
+import { SpeechService } from '../../services/speech/speech.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
   @ViewChild('searchForm') searchForm!: SearchFormComponent;
@@ -13,10 +15,10 @@ export class SearchComponent implements OnInit {
   searchResults: any;
   showMap: boolean = true;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private route: ActivatedRoute, private router: Router, private profileService: ProfileService, private speechService: SpeechService) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       const searchQuery = params['searchQuery'] || '';
       const communeQuery = params['communeQuery'] || '';
       // Ajoutez d'autres paramètres si nécessaire
@@ -25,11 +27,26 @@ export class SearchComponent implements OnInit {
         this.searchForm.setSearchParams(searchQuery, communeQuery);
       }
     });
+
+    const initialProfile = this.profileService.getCurrentProfile();
+    if (initialProfile?.systemPreferences?.voiceCommands) {
+      this.continousListening();
+    } else {
+      this.stopContinuousListening();
+    }
+  }
+
+  continousListening(): void {
+    console.log('Start listening button clicked');
+    this.speechService.continuousListening();
+  }
+
+  stopContinuousListening(): void {
+    this.speechService.stopContinuousListening();
   }
 
   onSearchResults(results: any): void {
     this.searchResults = results;
-
   }
 
   toggleView(): void {
